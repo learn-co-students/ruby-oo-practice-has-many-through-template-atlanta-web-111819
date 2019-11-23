7f5b5803e49c5b995492ef16c5c1b1295d129a5e
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-11-23 15:46:18 -0500

    Done.

--- lib/Model2.rb
+++ lib/Model2.rb
@@ -2,4 +2,22 @@
 #Feel free to change the name of the class
 class Model2
 
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def model1s
+        JoinModel.all.select {|join_model| join_model.model1 == self}
+    end
+
 end
